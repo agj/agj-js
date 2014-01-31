81c089b4069db3ee89fff12bdb004836d1a39540
agj <agj_chile@hotmail.com>
2014-01-31 13:15:09 -0300

    First commit.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/**
+ * Code by agj - http://www.agj.cl/
+ */
+
+require.config({
+	baseUrl: "js/lib"
+});
+
+require(["jquery", "agj/core"], function ($, AGJ) {
+	"use strict";
+
+	var trace = AGJ.trace;
+
+	//////////
+
+
+
+	//////////
+	
+
+	// Initialize.
+	
+	$(document).ready( function () {
+		
+	});
+
+});
+
