d59149e91b9aecb2b30eb482ef77967522af52d4
agj <agj_chile@hotmail.com>
2014-03-31 13:38:47 -0300

    Improved class-super.

--- src/agj/classes/class-super.js
+++ src/agj/classes/class-super.js
@@ -1,15 +1,18 @@
-
+/**
+ * Method for creating 'classes', based on John Resig's.
+ * Expanded to support '_super' method calls that properly go up the inheritance chain, and statics.
+ */
 define( function (require) {
 	'use strict';
 
-	var isFn = require('../is').fn;
+	var is = require('../is');
+	var to = require('../core').to;
+	var not = require('../function/not');
 
 	var module = function () {};
 
-	// This method of creating classes is a modification of John Resig's: http://ejohn.org/blog/simple-javascript-inheritance/
-	// My changes make the _super() method appropriately go up the inheritance chain, and allow for easy statics declaration.
 	var classInitializing = false;
-	var classUsesSuperTest = /xyz/.test(function(){'xyz';}) ? /\b_super\b/ : null;
+	var classUsesSuperTest = /xyz/.test(function(){ 'xyz'; }) ? /\b_super\b/ : null;
 
 	module.extend = function (properties) {
 		classInitializing = true;
@@ -18,24 +21,21 @@ define( function (require) {
 
 		var _super = this.prototype;
 
-		for (var name in properties) {
-			if (name === 'statics') continue;
-			if (isFn(properties[name]) && (!classUsesSuperTest || classUsesSuperTest.test(properties[name]))) {
-				prototype[name] = superify(properties[name]);
-			} else {
-				prototype[name] = properties[name];
-			}
-		}
+		properties.init = properties.init || function() {};
+		properties.cast = properties.cast || function(v) {
+			if (v instanceof Class) return v;
+			throw new TypeError("Invalid type casting.");
+		};
 
-		function superify(fn) {
-			return function () {
-				var originalSuper = this._super;
-				this._super = superFn;
-				var result = fn.apply(this, arguments);
-				this._super = originalSuper;
-				return result;
-			};
-		}
+		Object.keys(properties).filter(not(is.eq('statics'))).forEach( function (name) {
+			var method = properties[name];
+			if (is.fn(method) && (!classUsesSuperTest || classUsesSuperTest.test(method))) {
+				prototype[name] = superify(method, superFn);
+			} else {
+				prototype[name] = method;
+			}
+		});
+
 		function superFn(name) {
 			var scope = this;
 			return function () {
@@ -44,24 +44,21 @@ define( function (require) {
 		}
 
 		function Class() {
-			// All construction is actually done in the init method
 			if (this instanceof Class) {
-				if (!classInitializing && this.init)
+				// Instance construction.
+				if (!classInitializing)
 					this.init.apply(this, arguments);
 			} else {
-				// Not instantiating.
-				if (arguments.length === 1 && arguments[0] instanceof Class)
-					return arguments[0];
+				// Type casting.
+				if (arguments.length === 1)
+					return this.cast(arguments[0]);
 				throw new Error("Invalid type casting.");
 			}
 		}
 
-		if (properties.statics) {
-			for (name in properties.statics) {
-				if (properties.statics.hasOwnProperty(name))
-					Class[name] = properties.statics[name];
-			}
-		}
+		if (properties.statics) Object.keys(properties.statics).forEach( function (name) {
+			Class[name] = properties.statics[name];
+		});
 
 		properties = null;
 
@@ -71,6 +68,16 @@ define( function (require) {
 		Class.extend = module.extend;
 		return Class;
 	};
+
+	function superify(fn, superFn) {
+		return function () {
+			var originalSuper = this._super;
+			this._super = superFn;
+			var result = fn.apply(this, arguments);
+			this._super = originalSuper;
+			return result;
+		};
+	}
 
 	return module;
 
