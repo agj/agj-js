db3673931652b2b958e69e771ff31b271f17b321
agj <agj_chile@hotmail.com>
2014-04-04 14:31:00 -0300

    Removed commented-out code.

--- src/agj/function/maybe.js
+++ src/agj/function/maybe.js
@@ -9,21 +9,13 @@ define( function (require) {
 	var isFn      = require('../is').fn;
 	var toArray   = require('../utils/to-array');
 
-	var maybe = autoCurry( function (predicate, fn) {
+	var maybe = autoCurry( function maybe(predicate, fn) {
 		if (!isFn(predicate)) throw new TypeError("predicate is not a function.");
 		return function maybed(v) {
 			if (predicate(v)) return fn.apply(this, toArray(arguments));
 			return void 0;
 		};
 	});
-
-	// var maybe = autoCurry( function (predicate, elseValue, fn) {
-	// 	predicate = predicate || is.set;
-	// 	return function maybed() {
-	// 		var r = fn.apply(this, arguments);
-	// 		return predicate(r) ? r : elseValue;
-	// 	};
-	// });
 
 	return maybe;
 
