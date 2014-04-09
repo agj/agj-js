c95d709f2edde63005e753cbd9a1aa81ab571519
agj <agj_chile@hotmail.com>
2014-04-08 22:09:19 -0300

    Moved jasmine loading to AMD files.

--- tests/spec/main.js
+++ tests/spec/main.js
@@ -4,12 +4,36 @@ requirejs.config({
 	paths: {
 		'lib': '../lib',
 		'agj': '../../src/agj',
-		'rsvp': '../lib/rsvp'
-	}
+		'rsvp': '../lib/rsvp',
+		jasmine: '../lib/jasmine-2.0.0/jasmine',
+		jasmineHtml: '../lib/jasmine-2.0.0/jasmine-html',
+		jasmineBoot: '../lib/jasmine-2.0.0/boot',
+		// blanket: '../lib/blanket_jasmine',
+	},
+	shim: {
+		jasmine: {
+			exports: 'jasmine',
+		},
+		jasmineHtml: {
+			deps: ['jasmine'],
+			exports: 'jasmine',
+		},
+		jasmineBoot: {
+			deps: ['jasmine', 'jasmineHtml'],
+			exports: 'jasmine',
+		},
+		// blanket: {
+		// 	deps: ['jasmineBoot'],
+		// 	exports: 'blanket',
+		// },
+	},
 });
 
-require(['agj/extensions/rsvp'], function (promiseUtil) {
+require(['agj/extensions/rsvp', 'jasmineBoot'], function (promiseUtil, jasmine) {
 	'use strict';
+
+	// console.log(blanket);
+	// blanket.setFilter(['../src/agj/']);
 
 	promiseUtil.requireSequentially(
 		'array',
