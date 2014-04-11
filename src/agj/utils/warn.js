755ea9445987fdc6fb93ca32c2b8acdb589ffa2f
agj <agj_chile@hotmail.com>
2014-04-11 14:18:56 -0300

    Improved utils/warn.

--- src/agj/utils/warn.js
+++ src/agj/utils/warn.js
@@ -2,14 +2,7 @@
 define(function (require) {
 	'use strict';
 
-	function warn() {
-		if (console) {
-			if (console.warn)
-				console.warn.apply(console, toArray(arguments));
-			else if (console.log)
-				console.log.apply(console, toArray(arguments));
-		}
-	};
+	var warn = console && console.warn ? console.warn.bind(console) : console && console.log ? console.log.bind(console) : function () {};
 
 	return warn;
 
