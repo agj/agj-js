3656322b17b35d0d1bb5a0e7ead3a513d07fbb53
agj <agj_chile@hotmail.com>
2014-04-28 23:52:08 -0400

    Refactored function/autoCurry some.

--- src/agj/function/autoCurry.js
+++ src/agj/function/autoCurry.js
@@ -6,20 +6,17 @@ define( function (require) {
 	var toArray = require('../utils/toArray');
 
 	function autoCurryTo(arity, fn) {
-		if (arity > 0) {
-			return function autoCurried() {
-				if (arguments.length >= arity) {
-					return fn.apply(this, arguments);
-				} else {
-					var args = toArray(arguments);
-					return autoCurryTo(arity - args.length, function autoCurriedRest() {
-						return fn.apply(this, args.concat( toArray(arguments) ));
-					} );
-				}
-			};
-		} else {
-			return fn;
-		}
+		if (arity === 0) return fn;
+		return function autoCurried() {
+			if (arguments.length >= arity) {
+				return fn.apply(this, arguments);
+			} else {
+				var args = toArray(arguments);
+				return autoCurryTo(arity - args.length, function autoCurriedRest() {
+					return fn.apply(this, args.concat( toArray(arguments) ));
+				} );
+			}
+		};
 	}
 
 	var autoCurry = autoCurryArityFn(autoCurryTo);
