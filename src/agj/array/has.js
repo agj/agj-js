583330ca37168957113833653bed2d282af1e07f
agj <agj_chile@hotmail.com>
2014-09-04 20:14:05 -0400

    array/has and array/within refactored.

--- src/agj/array/has.js
+++ src/agj/array/has.js
@@ -2,23 +2,11 @@
 define( function (require) {
 	'use strict';
 
-	var overload = require('../function/overload');
-	var not = require('../function/not');
-	var is = require('../is');
-	var to = require('../to');
+	var autoCurry = require('../function/autoCurry');
 
-	function doHas(item, arr) {
+	var has = autoCurry( function (item, arr) {
 		return arr.indexOf(item) !== -1;
-	}
-
-	var has = overload(
-		[[not(is.equal(undefined)), is.set, overload.rest], doHas],
-		[[not(is.equal(undefined))], function (item) {
-			return function (arr) {
-				return doHas(item, arr);
-			};
-		}]
-	);
+	});
 
 	return has;
 	
--- src/agj/array/within.js
+++ src/agj/array/within.js
@@ -2,23 +2,11 @@
 define( function (require) {
 	'use strict';
 
-	var overload = require('../function/overload');
-	var not = require('../function/not');
-	var is = require('../is');
-	var to = require('../to');
+	var autoCurry = require('../function/autoCurry');
 
-	function doWithin(arr, item) {
+	var within = autoCurry( function (arr, item) {
 		return arr.indexOf(item) !== -1;
-	}
-
-	var within = overload(
-		[[is.set, not(is.equal(undefined)), overload.rest], doWithin],
-		[[is.set], function (arr) {
-			return function (item) {
-				return doWithin(arr, item);
-			};
-		}]
-	);
+	});
 
 	return within;
 	
