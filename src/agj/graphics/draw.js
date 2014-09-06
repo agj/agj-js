2a5b27a5c916a42888e5964997eb179eb975228d
agj <agj_chile@hotmail.com>
2014-09-05 22:39:45 -0400

    draw.curve can now draw cubic in addition to quadratic beziers.

--- src/agj/graphics/draw.js
+++ src/agj/graphics/draw.js
@@ -10,6 +10,9 @@ define( function (require) {
 
 	var parseRGB = require('../color/parseRGB');
 	var toHex = require('../number/inBase')(16);
+	var variadic = require('../function/variadic');
+	var first = require('../array/first');
+	var last = require('../array/last');
 
 	var draw = {
 		line: function (context2D, drawStyle, from, to) {
@@ -26,19 +29,23 @@ define( function (require) {
 			}
 		},
 
-		curve: function (context2D, drawStyle, from, mid, to) {
+		curve: variadic(function (context2D, drawStyle, points) {
+			var from = first(points);
+			var to = last(points);
 			if ((from.x !== to.x) || (from.y !== to.y)) {
 				context2D.save();
 				context2D.beginPath();
 				setLine(context2D, drawStyle);
 				context2D.moveTo(from.x, from.y);
-				context2D.quadraticCurveTo(mid.x, mid.y, to.x, to.y);
+				if (points.length === 3)      context2D.quadraticCurveTo(points[1].x, points[1].y, to.x, to.y);
+				else if (points.length === 4) context2D.bezierCurveTo(points[1].x, points[1].y, points[2].x, points[2].y, to.x, to.y);
+				else throw new Error("Wrong number of points passed.");
 				endLine(context2D, drawStyle);
 				context2D.restore();
 			} else {
 				drawPoint(context2D, drawStyle, from, to);
 			}
-		},
+		}),
 
 		circle: function (context2D, drawStyle, circ) {
 			context2D.save();
